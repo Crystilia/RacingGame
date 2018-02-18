27c6c01cac1b8262add38769586508d546cbc5cb
Mr-Nobody99 <Kutzy99@gmail.com>
2018-02-17 22:27:52 -0500

    Add Empty Project

---
+++ Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public string verticalAxisName = "Vertical";
+    public string horizontalAxisName = "Horizontal";
+    public string brakingKey = "Brake";
+
+    [HideInInspector] public float thrustInput;
+    [HideInInspector] public float TurnInput;
+    [HideInInspector] public bool isBraking;
+	
+	// Update is called once per frame
+	void Update () {
+        thrustInput = Input.GetAxis(verticalAxisName);
+        TurnInput = Input.GetAxis(horizontalAxisName);
+        isBraking = Input.GetButton(brakingKey);
+	}
+}
