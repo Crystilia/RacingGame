bce9e53d0158f0fcea5e0e9f7590e73c47e0bb5a
4h0 <35616167+4h0@users.noreply.github.com>
2018-02-19 05:02:39 -0500

    Add UI

---
+++ Racing_Game/Assets/Scripts/UI/LoadingScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//slider to show the loading progress
+//after finish loading -> take to sceneName that was preset as chara
+
+public class LoadingScene : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    private string sceneName = "Chara";
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(Loading(sceneName));
+    }
+
+    IEnumerator Loading(string loadScene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(loadScene);
+        operation.allowSceneActivation = false;
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            if (operation.progress == 0.9f)
+            {
+                Debug.Log("Press any key to start");
+                if (Input.anyKeyDown)
+                    operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Racing_Game/Assets/Scripts/UI/TeamScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//team logo show for 'timer' seconds -> changable
+//skip button show after 'timer' seconds -> take to loadingscene
+
+public class TeamScene : MonoBehaviour
+{
+    public Button skipButton;
+
+    private string loadScene = "Loading";
+
+    private float timer = 15f;
+
+    void Start()
+    {
+        skipButton = GetComponent<Button>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            skipButton.enabled = true;
+        }
+    }
+
+    public void SkipButton()
+    {
+        SceneManager.LoadScene(loadScene);
+    }
+}
