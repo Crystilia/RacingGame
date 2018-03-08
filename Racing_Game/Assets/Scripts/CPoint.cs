e36926842d2ee72de6370b449d7ff0ee192ce089
RomeoGD <36483734+RomeoGD@users.noreply.github.com>
2018-03-08 14:39:23 -0500

    App not working
    On my end it shows that I pushed the update, but looking on the github website it isn't there.

---
+++ Racing_Game/Assets/Scripts/CPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPoint : MonoBehaviour {
+
+	public GameObject LapFinish;
+	public GameObject CheckLapTrig;
+
+	void OnTriggerEnter () {
+		LapFinish.SetActive (true);
+		CheckLapTrig.SetActive (false);
+	}
+}
