ee652f034eccf43fab44810d52209fe597f4ce86
RomeoGD <36483734+RomeoGD@users.noreply.github.com>
2018-03-26 14:42:52 -0400

    Timer fix

--- Racing_Game/Assets/Scripts/SoundDelay.cs
+++ Racing_Game/Assets/Scripts/SoundDelay.cs
@@ -6,22 +6,31 @@ public class SoundDelay : MonoBehaviour {
 
     public AudioClip startSound;
     private AudioSource source;
+    public GameObject LapTimer;
 
 	// Use this for initialization
 	void Start () {
         source = GetComponent<AudioSource>();
         Invoke("StartTimer", 1.5f);
+        Invoke("EndTimer", 4.0f);
 	}
 
     void StartTimer()
     {
-
+        
         StartSound();
+
     }
 	
     public void StartSound()
     {
         source.PlayOneShot(startSound, 1.0f);
+        
+    }
+
+    public void EndTimer()
+    {
+        LapTimer.SetActive(true);
     }
 
 }
