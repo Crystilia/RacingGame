e9a129566e04d7125c64ed89333891677966ff3d
RomeoGD <36483734+RomeoGD@users.noreply.github.com>
2018-03-26 14:57:21 -0400

    Background Music

--- Racing_Game/Assets/Scripts/SoundDelay.cs
+++ Racing_Game/Assets/Scripts/SoundDelay.cs
@@ -5,32 +5,45 @@ using UnityEngine;
 public class SoundDelay : MonoBehaviour {
 
     public AudioClip startSound;
+    public AudioClip musicStart;
     private AudioSource source;
     public GameObject LapTimer;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         source = GetComponent<AudioSource>();
         Invoke("StartTimer", 1.5f);
         Invoke("EndTimer", 4.0f);
-	}
+        InvokeRepeating("PlayMyMusic", 4.0f, 0);
+    }
 
     void StartTimer()
     {
-        
+
         StartSound();
 
     }
-	
+
     public void StartSound()
     {
         source.PlayOneShot(startSound, 1.0f);
-        
+
     }
 
     public void EndTimer()
     {
         LapTimer.SetActive(true);
+        
+        
     }
 
+    public void PlayMyMusic ()
+    {
+        MusicStart();
+    }
+
+    public void MusicStart()
+    {
+        source.PlayOneShot(musicStart);
+    }
 }
