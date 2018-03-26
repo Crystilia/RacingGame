50ea659ad459f944fc0597e8476e92f3fc4d965d
RomeoGD <36483734+RomeoGD@users.noreply.github.com>
2018-03-26 14:31:36 -0400

    Countdown Sound

---
+++ Racing_Game/Assets/Scripts/SoundDelay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDelay : MonoBehaviour {
+
+    public AudioClip startSound;
+    private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        Invoke("StartTimer", 1.5f);
+	}
+
+    void StartTimer()
+    {
+
+        StartSound();
+    }
+	
+    public void StartSound()
+    {
+        source.PlayOneShot(startSound, 1.0f);
+    }
+
+}
